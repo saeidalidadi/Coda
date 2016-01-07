1962ad7e781fd6726839bc3f0bb45c327d17159c
Saeid Alidadi <alidadisaeid@gmail.com>
2016-01-07 12:24:20 +0330

    Some file of demo dir redefined

--- coda.js
+++ coda.js
@@ -3,38 +3,42 @@
  * Email : alidadisaeid@gmail.com
  * Date  : 26-Dec-2015
 */ 
-function Coda() {
-	// options for coda object
-	//the element for dancing
-	this.element = 'body';
++function(){
 
-	//Duration in ms for dancing
-	this.duration = 10;
+	window.Coda = function() {
+		// options for coda object
+		//the element for dancing
+		this.element = 'body';
 
-	//The range of for dancing;
-	this.colorRange = {
-		min: 0,
-		Max: 255
+		//Duration in ms for dancing
+		this.duration = 10;
+
+		//The range of for dancing;
+		this.colorRange = {
+			min: 0,
+			Max: 255
+		}
+		//The step that a color(R,G,B) chages in that range
+		this.colorStep = 1;
+		 
+		//Time step that a color(R,G,B) changes in that duration
+		this.timeStep = 10;
+
+		// Defines that the changing action will step down after getting to its Maximum value
+		this.backward = true;
+
+
 	}
-	//The step that a color(R,G,B) chages in that range
-	this.colorStep = 1;
-	 
-	//Time step that a color(R,G,B) changes in that duration
-	this.timeStep = 10;
 
-	// Defines that the changing action will step down after getting to its Maximum value
-	this.rollBack = true;
+	/* Coda methods */
+	//The color of that element will dance with calling this method
+	Coda.prototype.dance = function() {
+		console.log('this is a dance functiom');
+	}
 
-
-}
-
-/* Coda methods */
-//The color of that element will dance with calling this method
-coda.dance = function() {
-	
-}
-
-//This mwthod stop dancing of selected element
-coda.stop = function() {
-	
-}
+	//This mwthod stop dancing of selected element
+	Coda.prototype.stop = function() {
+		
+	}
+	var coda = new Coda();
+}();
---
+++ demo/js/script.js
@@ -0,0 +1,6 @@
+//initiate the options to define dancing style
+var option = {};
+
+//selecting body element and calling dancing method for it
+coda('body').dance();
+
