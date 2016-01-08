c7924f12f888e5ad4753aed7789601d80d3465d1
Saeid <Alidadi>
2016-01-08 19:54:21 +0330

    options for test added to script.js

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,7 +1,16 @@
 //initiate the options to define dancing style
-var option = {
-	
+var options = {
+	duration: 500,
+	redDur: 200,
+	greenDur: 300,
+	blueDur: 1000,
+	rColorStep: 30,
+	gColorStep: 20,
+	bColorStep: 4,
+	rTimeStep: 4,
+	bTimeStep: 100,
+	gTimeStep: 40
 };
-
+var du = 2000;
 //selecting body element and calling dancing method for it
-coda('body').dance(option);
+coda('body').dance(options);
