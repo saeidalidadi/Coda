b7ae506be24a511d10499071b5abe65dad660e72
Saeid Alidadi <alidadisaeid@gmail.com>
2016-01-30 20:29:55 +0330

    Ok, pasted options from new-way branch and now everything is ok, ha ha ha..

--- demo/js/script.js
+++ demo/js/script.js
@@ -2,10 +2,19 @@
 var options = {
 
 	all : { duration: 30000, colorStep: 30,timeStep: 70, colorRange : [0, 255], backward: false },
-	redOpt: { duration: 10000, colorStep: 30,timeStep: 70, colorRange : [0, 255], backward: false },
-	greenOpt: { duration: 10000, colorStep: 30,timeStep: 70, colorRange : [0, 255], backward: false },
-	blueOpt : { duration: 10000, colorStep: 30,timeStep: 70, colorRange : [0, 255], backward: false },
-	hasRed: false
+	redOpt: { duration: 5000, colorStep: 1, timeStep: 10,
+		backward:  true,
+		colorRange: [60 , 30] },
+	greenOpt: { duration: 5000, 
+		colorStep: 1,
+		timeStep: 10,
+		backward: true,
+		colorRange: [30, 150] },
+	blueOpt : { duration:5000, 
+		colorStep: 1, 
+		timeStep: 100, 
+		backward: true,
+		colorRange: [30, 150] }
 };
 
 //selecting body element and calling dancing method for it
