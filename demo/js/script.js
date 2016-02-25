050eb081707725e55c30389e47cf2bfce1b87c2c
Arasham <Arasham>
2016-02-26 01:37:50 +0330

    Added values from inputs to options in script.js. Need more. This is #1

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,22 +1,64 @@
 //initiate the options to define dancing style
+var red_opt = document.getElementById("red-duration-val");
 var options = {
 
-	all : { duration: 30000, colorStep: 30,timeStep: 70, colorRange : [0, 255], backward: false },
-	redOpt: { duration: 5000, colorStep: 1, timeStep: 10,
-		backward:  true,
-		colorRange: [60 , 30] },
-	greenOpt: { duration: 5000, 
-		colorStep: 1,
-		timeStep: 10,
-		backward: true,
-		colorRange: [30, 150] },
-	blueOpt : { duration:5000, 
-		colorStep: 1, 
-		timeStep: 100, 
-		backward: true,
-		colorRange: [30, 150] }
+	all : { 
+		duration: document.getElementById("duration-val"),
+		colorStep: document.getElementById("colorstep"),
+		timeStep: document.getElementById("timestep"),
+		colorRange : [document.getElementById("range-min"), document.getElementById("range-max")],
+		backward: document.getElementById("red-backward-state")
+	},
+	redOpt: { 
+		duration: document.getElementById("red-duration-val"),
+		colorStep: document.getElementById("red-colorstep"),
+		timeStep: document.getElementById("red-timestep"),
+		colorRange : [document.getElementById("red-range-min"), document.getElementById("red-range-max")],
+		backward: document.getElementById("red-backward-state") 
+	},
+	greenOpt: { 
+		duration: document.getElementById("green-duration-val"),
+		colorStep: document.getElementById("green-colorstep"),
+		timeStep: document.getElementById("green-timestep"),
+		colorRange : [document.getElementById("green-range-min"), document.getElementById("green-range-max")],
+		backward: document.getElementById("green-backward-state") 
+	},
+	blueOpt : { 
+		duration: document.getElementById("blue-duration-val"),
+		colorStep: document.getElementById("blue-colorstep"),
+		timeStep: document.getElementById("blue-timestep"),
+		colorRange : [document.getElementById("blue-range-min"), document.getElementById("blue-range-max")],
+		backward: document.getElementById("blue-backward-state")
+	}
 };
 
 //selecting body element and calling dancing method for it
-//var coda = new Coda('body', options);
-coda('body', options).dance();
+window.onload = function() {
+
+	document.getElementById("btn-common").onclick = function() {
+		coda('body', options).dance();
+	}
+
+	document.getElementById("btn-red").onclick = function() {
+		var opt = {
+			redOpt: { 
+				duration: document.getElementById("red-duration-val"),
+				colorStep: document.getElementById("red-colorstep"),
+				timeStep: document.getElementById("red-timestep"),
+				colorRange : [document.getElementById("red-range-min"), document.getElementById("red-range-max")],
+				backward: document.getElementById("red-backward-state") 
+			},
+		}
+
+		coda('body', opt).dance();
+	}
+
+	document.getElementById("btn-green").onclick = function() {
+		coda('body', options).dance();
+	}
+
+	document.getElementById("btn-blue").onclick = function() {
+		coda('body', options).dance();
+	}
+}
+	
